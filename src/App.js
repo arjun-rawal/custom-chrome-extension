f552f007a7170c9ff3b10c7c2d557cc172799741
arjun-rawal <arawalarjun@gmail.com>
2023-03-19 21:41:07 -0400

    chat gpt implementation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import { NativeBaseProvider, Box, Center } from "native-base";
 import DateTime from "./components/dateTime";
+import ChatGPT from "./components/chatGpt";
 function App() {
   return (
     <NativeBaseProvider>
-      <Center fontSize={30} style={{ fontWeight: "bold", fontFamily: "arial" }}>
+      {/* <Center fontSize={30} style={{ fontWeight: "bold", fontFamily: "arial" }}>
         <DateTime type="date" />
       </Center>
-      <Center stlye={{fontFamily: "arial",}} fontSize={50} marginTop={"13%"}>
+      <Center style={{fontFamily: "arial"}} fontSize={50} marginTop={"13%"}>
         <DateTime type="time" />
-      </Center>
+      </Center> */}
+    <ChatGPT />
     </NativeBaseProvider>
   );
 }
