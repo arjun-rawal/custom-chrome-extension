f94f5dae5b98c71a1cec19830ed76d50a3dbbd1f
arjun-rawal <arawalarjun@gmail.com>
2023-03-27 21:12:56 -0400

    Scrapped chatgpt
    Too difficult😭

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
 import { NativeBaseProvider, Box, Center } from "native-base";
 import DateTime from "./components/dateTime";
-import ChatGPT from "./components/chatGpt";
 function App() {
   return (
     <NativeBaseProvider>
-      {/* <Center fontSize={30} style={{ fontWeight: "bold", fontFamily: "arial" }}>
+      <Center fontSize={30} style={{ fontWeight: "bold", fontFamily: "arial" }}>
         <DateTime type="date" />
       </Center>
       <Center style={{fontFamily: "arial"}} fontSize={50} marginTop={"13%"}>
         <DateTime type="time" />
-      </Center> */}
-    <ChatGPT />
+      </Center>
     </NativeBaseProvider>
   );
 }
