94ff827c70d3e670c37f1893dc0ba77baf5ad63b
arjun-rawal <arawalarjun@gmail.com>
2023-03-27 21:55:29 -0400

    added amplify auth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import "./App.css";
-import { Button, MantineProvider } from '@mantine/core';
+import { Button, Center, MantineProvider } from '@mantine/core';
 
 import DateTime from "./components/dateTime";
+import { Amplify } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 function App() {
   return (
       <MantineProvider>
-      <div style={{ fontSize:'30px',fontWeight: "bold", fontFamily: "arial" }}>
-        <DateTime type="date" />
-      </div>
-      <div style={{fontFamily: "arial", fontSize:'50px',marginTop:"20%"}} >
-        <DateTime type="time" />
-      </div>
-      <Button>
+      <Button style={{position:'absolute'}} right={15} top={15}>
         Sign in
       </Button>
+      <Center style={{ fontSize:'30px',fontWeight: "bold", fontFamily: "arial" }}>
+        <DateTime type="date" />
+      </Center>
+      <Center style={{fontFamily: "arial", fontSize:'50px',marginTop:"13%"}} >
+        <DateTime type="time" />
+      </Center>
+
       </MantineProvider>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
