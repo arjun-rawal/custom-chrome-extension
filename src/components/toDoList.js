753b21afef017921dfe3e815128e0ebbee45a84c
arjun-rawal <arawalarjun@gmail.com>
2023-04-10 14:01:41 -0400

    todolist functionality

--- src/components/toDoList.js
+++ src/components/toDoList.js
@@ -1,7 +1,23 @@
-import { Box, Button, Center, Flex, TextInput } from "@mantine/core";
+import { Box, Button, Center, Flex, Grid, Stack, TextInput } from "@mantine/core";
 import { IconListDetails,IconArrowNarrowDown} from "@tabler/icons-react";
+import { useState } from "react";
 
 function ToDoList(props) {
+  const [tasks,setTasks] = useState([])
+  const [value, setValue] = useState('');
+
+  function newTask(){
+    if (value.length !==0){
+      let temp = [value]
+      setTasks([...tasks,temp])
+      setValue("")
+    }
+  }
+  const listItems = tasks.map((task) =>
+  <li key={task}>
+    {task}
+  </li>
+);
   return (
     <Box style={{ width: props.width, position: "absolute", right: "10pt" }}>
       <Center>To Do List</Center>
@@ -13,12 +29,15 @@ function ToDoList(props) {
           placeholder="Task"
           variant="filled"
           style={{width:'100%'}}
-       
+          value={value} onChange={(event) => setValue(event.currentTarget.value)}
         />
-        <Button>
+        <Button onClick={newTask}>
           <IconArrowNarrowDown/>
         </Button>
       </Flex>
+      <Grid columns={5}>
+      <ul>{listItems}</ul>
+      </Grid>
     </Box>
   );
 }
